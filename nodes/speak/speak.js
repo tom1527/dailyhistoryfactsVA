92107d5ef5b66a31a72e86213d09fdbaab5b25f9
tom1527 <70600248+tom1527@users.noreply.github.com>
2022-03-14 13:20:02 +0000

    Include suggestions option in speak.js

--- nodes/speak/speak.js
+++ nodes/speak/speak.js
@@ -6,10 +6,16 @@ module.exports = function(RED) {
         RED.nodes.createNode(this, n);
         var node = this;
         node.text = n.text;
+        node.suggestions = n.suggestions;
         this.on("input", function (msg, send, done) {
           if (msg.req) {
             var renderedMsg = mustache.render(node.text, msg);
             msg.req.ask(renderedMsg);
+            if (msg.req.isGoogleAction()) {
+                if (node.suggestions.length) {  
+                    msg.req.$googleAction.showSuggestions(node.suggestions);   
+                }
+            }
           }
           send(msg);
         });
