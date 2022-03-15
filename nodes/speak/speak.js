1939edf7c8406c3f29412792c2b1ea6e5900dccb
tom <twbradley@hotmail.com>
2022-03-15 00:20:48 +0000

    Fix suggestions issue

--- nodes/speak/speak.js
+++ nodes/speak/speak.js
@@ -7,14 +7,17 @@ module.exports = function(RED) {
         var node = this;
         node.text = n.text;
         node.suggestions = n.suggestions;
-        const suggestions = node.suggestions.split(', ');
+
         this.on("input", function (msg, send, done) {
+          if(node.suggestions) {
+            msg.suggestions = node.suggestions.split(', ');
+          }
           if (msg.req) {
             var renderedMsg = mustache.render(node.text, msg);
             msg.req.ask(renderedMsg);
             if (msg.req.isGoogleAction()) {
-                if (node.suggestions.length) {  
-                    msg.req.$googleAction.showSuggestions(suggestions);   
+                if (node.suggestions) {  
+                    msg.req.$googleAction.showSuggestions(msg.suggestions);   
                 }
             }
           }
